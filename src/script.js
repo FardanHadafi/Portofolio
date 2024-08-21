7cc30b3c2bc34018229518f0de6c2302857fc0ca
Fardan Hadafi <fardan.hadafi@yahoo.com>
2024-08-21 20:26:23 +0700

    Added functionality and dark mode

--- src/script.js
+++ src/script.js
@@ -11,10 +11,44 @@ hamburger.addEventListener('click', () => {
 window.onscroll = function() {
   const header = document.querySelector('header');
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector('#toTop');
 
   if (window.scrollY > fixedNav) {
     header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
+    toTop.classList.add('block');
   } else {
     header.classList.remove('navbar-fixed');
+    toTop.classList.remove('block');
+    toTop.classList.add('hidden');
+  };
+};
+
+// Klik diluar hamburger
+window.addEventListener('click', e => {
+  if(e.target != navMenu && e.target != hamburger) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
   }
+});
+
+// Dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', () => {
+ if (darkToggle.checked) {
+  html.classList.add('dark');
+  localStorage.theme = 'dark';
+ } else {
+  html.classList.remove('dark');
+  localStorage.theme = 'light';
+ }
+});
+
+// Pindahkan toggle sesuai mode 
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
 }
